f4f6753a0a8169e51c537d89c60997012e00a081
Nurislam Ab <you@example.com>
2020-07-30 16:27:31 +0500

    Add basic auth for the users

--- db/migrate/20200730072328_create_users.rb
+++ db/migrate/20200730072328_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :email,    null: false
       t.string :name,     null: false
-      t.string :surname,  null: false
-      t.string :avatar,   null: false
+      t.string :surname,  null: true
+      t.string :avatar,   null: true
       t.datetime "created_at", precision: 6, null: false
       t.datetime "updated_at", precision: 6, null: false
     end
