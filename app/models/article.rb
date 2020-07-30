d3233ed750a838ee721febdcdcf55587769a161b
Nurislam Ab <you@example.com>
2020-07-30 21:10:06 +0500

    Add category and article category models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    
+    has_one :article_category
+    has_one :category, through: :article_category, dependent: :destroy
 end
