c764a7e48d499d4a2d6e3d2de1984528b877220a
Nurislam Ab <you@example.com>
2020-07-30 19:55:59 +0500

    Add association in the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_072328) do
+ActiveRecord::Schema.define(version: 2020_07_30_113227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "title"
+    t.text "text"
+    t.string "image"
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_articles_on_author_id"
+    t.index ["id"], name: "index_articles_on_id"
+    t.index ["title"], name: "index_articles_on_title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
