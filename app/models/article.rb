799e07c417459fe6c1f620097e997cad298fc7c5
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-07-31 10:58:50 +0500

    Fix the category relationship with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    
-    has_one :article_category
-    has_one :category, through: :article_category, dependent: :destroy
+
+    belongs_to :category
 end
