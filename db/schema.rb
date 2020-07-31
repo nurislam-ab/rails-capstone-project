4140c7403b94196f6d1135bec9fa1a6737577d37
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-07-31 11:00:29 +0500

    Migrate db for category_id column in the articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_152822) do
+ActiveRecord::Schema.define(version: 2020_07_31_052429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_152822) do
     t.string "image"
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "created_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["id"], name: "index_articles_on_id"
     t.index ["title"], name: "index_articles_on_title"
