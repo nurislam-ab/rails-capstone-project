d01a99d97f25513f0b214fd5195fd6e46e79147a
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-08-05 01:09:24 +0500

    Add require login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    private
+ 
+    def require_login
+        unless logged_in?
+            flash[:error] = "You must be logged in to access this section"
+            redirect_to login_path
+        end
+    end
 end
