41ae89d6a77660aa28b473b2b4055e327096107e
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-08-07 04:22:35 +0500

    Add icons and update seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,5 @@
-# frozen_string_literal: true
-
-# Class documentation comment
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 categories = Category.create([
-                               { id: 4, name: 'Holidays', priority: 2 },
+                               { id: 4,     name: 'Holidays', priority: 2 },
                                { id: 8,     name: 'Food news',      priority: 0 },
                                { id: 9,     name: 'How-tos',        priority: 0 },
                                { id: 11,    name: 'Reviews',        priority: 1 },
