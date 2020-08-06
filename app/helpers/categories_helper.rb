dca61238e4a675ad4de781dff45e41ed7d452b54
Nurislam Ab <you@example.com>
2020-08-06 14:46:11 +0500

    Fix offences and add category_blocks method

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 # CategoryHelper
 module CategoriesHelper
   def show_categories(categories)
     category_preview = ''
     categories.collect do |c|
-      next unless c.articles.count > 0
+      next unless c.articles.count.positive?
 
       category_preview += <<-HTML
                     <article class='featured-article featured-article-category'>
@@ -21,5 +19,18 @@ module CategoriesHelper
       HTML
     end
     category_preview.html_safe
+  end
+
+  def category_blocks(categories)
+    html_block = ''
+    categories.collect do |c|
+      html_block += <<-HTML
+      <div class="category-box">
+        #{link_to(c.name, c)}
+      </div>
+      HTML
+    end
+
+    html_block.html_safe
   end
 end
