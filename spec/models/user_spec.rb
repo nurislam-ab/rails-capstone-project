53ce2e85c05ae171be3d802409e1de0012680827
Nurislam Ab <you@example.com>
2020-08-06 15:48:32 +0500

    Test models (user, vote, article, category)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,11 +10,6 @@ RSpec.describe User, type: :model do
       expect { User.create! }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
-    it do
-      create(:user)
-      should validate_uniqueness_of(:email).ignoring_case_sensitivity
-    end
-
     it 'should register user successfuly' do
       user = User.new(name: 'test user', email: 'mail@mail.com').save
       expect(user).to be true
