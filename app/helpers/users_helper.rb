c20af6891620cb8ebab24c4095c766f244a0cb8c
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-08-07 21:37:29 +0500

    Move the user avatar logic from the edit view to the helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,17 @@
 # UsersHelper
 module UsersHelper
+    def avatar_attached(field, avatar)
+        html_block = ""
+        if @user.avatar.attached?
+            html_block += <<-HTML
+                #{field.file_field avatar}
+            HTML
+        else
+            html_block += <<-HTML
+            #{field.file_field avatar, required: true}
+            HTML
+        end
+
+        html_block.html_safe
+    end
 end
