920b69cb1fae072e7a462ae66711f52c23864eb7
Nurislam Ab <you@example.com>
2020-08-07 19:48:19 +0500

    Show all the categories with the most recent article

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @most_voted_articles = Vote.most_voted_articles
     @recent_articles = Article.with_attached_image.includes(:category, :author).recent_articles
-    @categories = Category.categories_by_priority.includes(articles: %i[author votes]).first(4)
+    @categories = Category.categories_by_priority.includes(articles: %i[author votes]).all
     @articles = Article.with_attached_image.includes(:category, :author, :votes).all.order('created_at desc')
   end
 end
